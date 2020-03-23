d265c9c87bf624105412e117fc2fc7350c8952a5
abdul <13372558@student.uts.edu.au>
2020-03-24 05:10:02 +1100

    Initial Homepage (Login page) design.

--- app/src/main/java/com/example/healthapp/MainActivity.java
+++ app/src/main/java/com/example/healthapp/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.healthapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1, rellay2;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+        handler.postDelayed(runnable, 4000); //2000 is the timeout for the splash screen
+
+        // Button listeners
+        Button forgotPassBtn = (Button) findViewById(R.id.forgotPasswordBtn);
+        Button signupBtn = (Button) findViewById(R.id.signUpBtn);
+
+        forgotPassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
+
+
+
+
+
+
 }
