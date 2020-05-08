5dfb3b2356003000bf9107c7549e9d12a75869fc
lucky <jaiswal.lucky8888@gmail.com>
2020-05-08 16:10:48 +1000

    Updted signout functionality in correct manner to user firebase auth log out

--- app/src/main/java/com/example/healthapp/AdminDashboard.java
+++ app/src/main/java/com/example/healthapp/AdminDashboard.java
@@ -7,15 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AdminDashboard extends AppCompatActivity {
     private Button btn_viewRequests,btn_Logout;
-
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
         btn_viewRequests=findViewById(R.id.viewRequests);
         btn_Logout=findViewById(R.id.logout);
+        firebaseAuth=FirebaseAuth.getInstance();
 
         btn_viewRequests.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +32,7 @@ public class AdminDashboard extends AppCompatActivity {
             public void onClick(View v) {
                 btn_Logout.setText("Logging out...");
                 btn_Logout.setEnabled(false);
+                firebaseAuth.signOut();
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
         });
