17866b034a2ffaab1688fa705cd2acddfaabef2c
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2020-05-31 21:45:18 +1000

    Updated homepage layout, other small defects

--- app/src/main/java/com/example/healthapp/DoctorDashboard.java
+++ app/src/main/java/com/example/healthapp/DoctorDashboard.java
@@ -1,16 +1,25 @@
 package com.example.healthapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class DoctorDashboard extends AppCompatActivity implements View.OnClickListener {
     private CardView layoutMessages;
     private CardView patientList;
+    private Button logoutBtn;
+    //private FirebaseAuth firebaseAuth;
+    private ImageView doctor_logout;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +28,22 @@ public class DoctorDashboard extends AppCompatActivity implements View.OnClickLi
         patientList = (CardView) this.findViewById(R.id.patientList);
         layoutMessages.setOnClickListener(this);
         patientList.setOnClickListener(this);
+        doctor_logout = findViewById(R.id.dashboardImg);
+        //firebaseAuth=FirebaseAuth.getInstance();
+
+        //logoutBtn = findViewById(R.id.dashboardImg);
+
+        doctor_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
 
     }
+
+
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
